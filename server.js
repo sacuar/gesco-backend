85f686e8a9eb5b05bcee63dff7d687e8738041dc
SteveMelons <oli.olioli.fischer@gmail.com>
2021-01-27 19:37:49 +0100

    Added port env

--- server.js
+++ server.js
@@ -98,5 +98,5 @@ low(adapter)
     return db.defaults({ users: [] }).write();
   })
   .then(() => {
-    app.listen(3000, () => console.log("listening on port 3000"));
+    app.listen(process.env.PORT || 3000, () => console.log("listening"));
   });
